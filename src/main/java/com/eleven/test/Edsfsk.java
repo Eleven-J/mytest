9fd66b8a9c2980dd57e71ab6c1aa086400253a79
jackiez <jiezhuang@bupt.edu.cn>
2021-07-22 19:21:00 +0800

    master1

--- src/main/java/com/eleven/test/Edsfsk.java
+++ src/main/java/com/eleven/test/Edsfsk.java
@@ -2,8 +2,6 @@ package com.eleven.test;
 
 public class Edsfsk {
     public static void main(String[] args) {
-        System.out.println("dkdkkd");
-        System.out.println("jjjjj");
-        System.out.println("hotfix分支修改的内容");
+        System.out.println("master1");
     }
 }
