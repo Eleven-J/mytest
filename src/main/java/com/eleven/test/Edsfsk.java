f719646824e28e4c22e032b51d1136a12fd05900
jackiez <jiezhuang@bupt.edu.cn>
2021-07-22 19:54:37 +0800

    else1

--- src/main/java/com/eleven/test/Edsfsk.java
+++ src/main/java/com/eleven/test/Edsfsk.java
@@ -4,5 +4,6 @@ public class Edsfsk {
     public static void main(String[] args) {
         System.out.println("master1");
         System.out.println("master2");
+        System.out.println("else1");
     }
 }
