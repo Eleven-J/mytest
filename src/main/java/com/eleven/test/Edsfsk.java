ceadb4860fd74f87761103472f6aa7fed83626c0
jackiez <jiezhuang@bupt.edu.cn>
2021-07-22 17:14:14 +0800

    version5

--- src/main/java/com/eleven/test/Edsfsk.java
+++ src/main/java/com/eleven/test/Edsfsk.java
@@ -3,5 +3,6 @@ package com.eleven.test;
 public class Edsfsk {
     public static void main(String[] args) {
         System.out.println("dkdkkd");
+        System.out.println("hotfix分支修改的内容");
     }
 }
