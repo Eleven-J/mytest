1d08e0325a3da77e786959e0463fd57de1f58bf7
jackiez <jiezhuang@bupt.edu.cn>
2021-07-22 19:22:42 +0800

    master2

--- src/main/java/com/eleven/test/Edsfsk.java
+++ src/main/java/com/eleven/test/Edsfsk.java
@@ -3,5 +3,6 @@ package com.eleven.test;
 public class Edsfsk {
     public static void main(String[] args) {
         System.out.println("master1");
+        System.out.println("master2");
     }
 }
